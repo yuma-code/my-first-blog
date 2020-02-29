a6f4c7a0391ede30b4b541879fdbad9929dae692
sitatake <tkstdic@gmail.com>
2020-02-29 14:21:06 +0900

    last djangogirls

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,11 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-LOGIN_REDIRECT_URL = '/'
