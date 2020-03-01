6d04f94400793400ba266bb984c253b5cdb9abc2
yuma-code <onanukintaro@gmail.com>
2020-03-01 17:19:38 +0900

    maa

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Post,Comment
 from .forms import PostForm,CommentForm
 
+def start(request):
+    return render(request, 'blog/実験.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
