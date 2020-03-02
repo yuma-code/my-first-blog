ce9f567355197f32c73232a236f361956546091f
yuma-code <onanukintaro@gmail.com>
2020-03-02 13:27:15 +0900

    had

--- blog/views.py
+++ blog/views.py
@@ -61,7 +61,7 @@ def post_publish(request, pk):
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post_list')
+    return redirect('comeon')
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
