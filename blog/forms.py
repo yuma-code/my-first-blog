61a25ad7b0c5d3850302044e9b58912e627af93e
yuma-code <onanukintaro@gmail.com>
2020-03-18 22:13:04 +0900

    add device

--- blog/forms.py
+++ blog/forms.py
@@ -2,11 +2,22 @@ from django import forms
 
 from .models import Post, Comment
 
+GENDER_CHOICES=(
+    ('ps4','PS4'),
+    ('pc','PC')
+)
+
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+    gender_r = forms.ChoiceField(
+        label='device',
+        widget=forms.Select,
+        choices=GENDER_CHOICES,
+        required=True,
+    )
 
 class CommentForm(forms.ModelForm):
 
